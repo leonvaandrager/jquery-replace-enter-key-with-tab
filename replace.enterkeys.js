8bfab4b0f8a5e897e74901d0e0844d60525f383e
unknown <leon@leon-PC.(none)>
2011-12-26 23:12:12 +0100

    first init version

---
+++ replace.enterkeys.js
@@ -0,0 +1,52 @@
+﻿(function ($) {
+    $.fn.replaceEnters = function () {
+
+        function checkForEnter(event) {
+            if (event.keyCode == 13) {
+                //validation
+                try {
+                    if (!$(this).valid()) {
+                        return true;
+                    }
+                } catch (e) {
+
+                }
+
+                var inputs = $(":input").not(":hidden").not(":button").not(":submit");
+                var currentInputNumber = inputs.index(this);
+                var input;
+                if (inputs[currentInputNumber + 1] != null) {
+                    input = inputs[currentInputNumber + 1];
+                    selectInput(input);
+                } else if (inputs.length > 0) {
+                    input = inputs[0];
+                    selectInput(input);
+                }
+                event.preventDefault();
+                return false;
+            }
+        }
+        function selectInput(input) {
+            input.focus();
+            //Can go wrong on select inputs  
+            try {
+                input.select();
+            } catch (ex) {
+            }
+        }
+
+        return this.each(function () {
+
+
+            if ($(this).is(":input")) {
+                if ($.browser.mozilla) {
+                    $(this).keypress(checkForEnter);
+                } else {
+                    $(this).keydown(checkForEnter);
+                }
+            }
+
+
+        });
+    };
+})(jQuery);
